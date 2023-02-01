b51e0c353697d3970bd0c263308bf2d12bf64ed9
Pawel Stanke <pablostanke21@gmail.com>
2023-02-02 00:29:41 +0100

    Remove client model

--- models/client.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const clientSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Client', clientSchema);
